c4095f7ab97bc73e9f3be19f6edab595899f2d94
Lenno Nagel <lenno@nagel.ee>
2020-01-25 09:22:24 +0200

    Initial commit

---
+++ mp4rename.py
@@ -0,0 +1,47 @@
+import datetime
+
+import pytz as pytz
+import os
+import re
+import sys
+from pymediainfo import MediaInfo
+
+
+def do_rename(filename):
+    tagged_date = get_tagged_date(filename=filename)
+    if tagged_date:
+        extension = re.search('\.([a-zA-Z0-9]+)$', filename).groups()[0].lower()
+        tagged_date_local = tagged_date.astimezone(tz=pytz.timezone('Europe/Brussels'))
+        new_filename = format_filename(timestamp=tagged_date_local, extension=extension)
+        new_abs_filename = os.path.abspath(os.path.join(filename, os.pardir, new_filename))
+        if filename != new_filename:
+            print(filename, '->', new_filename)
+            os.rename(filename, new_abs_filename)
+        else:
+            print(filename, ' (not changed)')
+    else:
+        raise RuntimeError('Could not determine tagged date for {}'.format(filename))
+
+
+def get_tagged_date(filename):
+    media_info = MediaInfo.parse(filename=filename)
+    for track in media_info.tracks:
+        if hasattr(track, 'tagged_date'):
+            return parse_date_string(track.tagged_date)
+    return None
+
+
+def parse_date_string(value):
+    return datetime.datetime.strptime(value, "UTC %Y-%m-%d %H:%M:%S").replace(tzinfo=pytz.timezone('UTC'))
+
+
+def format_filename(timestamp, fmt='%Y%m%d_%H%M%S', extension='mp4'):
+    return '{name}.{extension}'.format(
+        name=datetime.datetime.strftime(timestamp, fmt),
+        extension=extension,
+    )
+
+
+if __name__ == '__main__':
+    for filename in sys.argv[1:]:
+        do_rename(filename)
---
+++ portrait_mover.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+import logging
+import re
+import shutil
+import sys
+from pathlib import Path
+
+logger = logging.getLogger("portrait_mover")
+
+
+def check_dir(path):
+    if path.is_dir():
+        if re.match("^IMG_[0-9]{8}_[0-9]{6}$", path.name):
+            do_move(path.parent, path)
+        else:
+            # scan members
+            for item in path.iterdir():
+                check_dir(item)
+
+
+def do_move(base_dir, portrait_dir):
+    # collect files to move
+    candidates = []
+    removables = []
+    others = []
+    for item in portrait_dir.iterdir():
+        if re.match("^(00000|00100)(.*)(IMG|PORTRAIT)_(00000|00100)_BURST([0-9]+)(|_COVER).jpg$", item.name):
+            candidates.append(item)
+        elif item.name in {".deletemarkers"}:
+            removables.append(item)
+        else:
+            others.append(item)
+
+    target = base_dir / f"{portrait_dir.name}.jpg"
+
+    if not target.exists():
+        if len(candidates) == 1 and len(others) == 0:
+            logger.info(f"MV {base_dir}/{{{candidates[0].relative_to(base_dir)} -> {target.relative_to(base_dir)}}}")
+            candidates[0].rename(target)
+        elif len(candidates) > 1 or len(others) > 0:
+            logger.debug(f"?? {portrait_dir}")
+    for removable in removables:
+        logger.debug(f"RM {removable}")
+        shutil.rmtree(removable)
+    if len(list(portrait_dir.iterdir())) == 0:
+        logger.debug(f"RM {portrait_dir}")
+        portrait_dir.rmdir()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    for path_str in sys.argv[1:]:
+        check_dir(Path(path_str))
